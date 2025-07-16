1c5eee08c2a9ace24e5dae79010f709e5069da91
Zain-Ali-712 <za9690000@gmail.com>
2025-07-16 16:38:20 +0500

    Error Handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+
+function getRandomInt(count:number){
+    return Math.floor(Math.random()*count);
+}
+
 export default async function ReviewId({
     params,
 }:{
     params: Promise<{productId:string, reviewId:string}>;
 }) {
+    const num =getRandomInt(2);
+    if (num===1){
+        throw new Error ("Error loading Review!");
+    }
     const {productId, reviewId} = await params;
     return <h1><Link href={`/products/${productId}/reviews/${reviewId}`}>Review {reviewId} for Product {productId}</Link></h1>
     
